0dfd58b6ea47a43adfc38daf89e37f49413c8cf2
VENKATASAI KOMMU <87709217+kar-thik018@users.noreply.github.com>
2022-10-30 02:10:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,24 +35,7 @@ def predict():
 
 
 
-# prediction function
-def ValuePredictor(to_predict_list):
-	to_predict = np.array(to_predict_list).reshape(1, 12)
-	loaded_model = pickle.load(open("model.pkl", "rb"))
-	result = loaded_model.predict(to_predict)
-	return result[0]
-
-@app.route('/result', methods = ['POST'])
-def result():
-	if request.method == 'POST':
-		to_predict_list = request.form.to_dict()
-		to_predict_list = list(to_predict_list.values())
-		to_predict_list = list(map(int, to_predict_list))
-		result = ValuePredictor(to_predict_list)	
-		
-		return render_template("result.html", prediction = result)
-
 
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=9696)
+    app.run(debug=True)
